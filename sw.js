e3617c95fb8e1f098c21f77d1227e4ac3d371727
Murali V <murali.v@partner.samsung.com>
2021-12-17 14:35:18 +0530

    initial3

--- sw.js
+++ sw.js
@@ -1,36 +1,23 @@
 self.addEventListener('push', function(e) {
   console.log("e",e);
-  let data = e.data.json();
-  // var options = {
-  //   body: 'This notification was generated from a push!',
-  //   icon: 'images/example.png',
-  //   vibrate: [100, 50, 100],
-  //   data: {
-  //     dateOfArrival: Date.now(),
-  //     primaryKey: '2'
-  //   },
-  //   actions: [
-  //     {action: 'explore', title: 'Explore this new world',
-  //       icon: 'images/checkmark.png'},
-  //     {action: 'close', title: 'Close',
-  //       icon: 'images/xmark.png'},
-  //   ]
-  // };
-  var title = 'Samsung.com.';
-  var body = 'Order :'+ data.po_id+ " status: "+ data.event_type;
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
   e.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
+    self.registration.showNotification('Hello world!', options)
   );
-  // e.waitUntil(
-  //   self.registration.showNotification('Hello world!', options)
-  // );
 });
 
 self.addEventListener('notificationclick', function(event) {
